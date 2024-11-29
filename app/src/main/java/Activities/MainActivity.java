f2c71cb7a99c9f9a6b089beb87ad90bfba5be4f7
Hoang0810 <tuanhaps37570@fpt.edu.vn>
2024-11-29 13:58:21 +0700

    Implement API to Confirm and Cancel Order

--- app/src/main/java/Activities/MainActivity.java
+++ app/src/main/java/Activities/MainActivity.java
@@ -31,7 +31,7 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         initUI();
-//        setupBottomNav();
+        setupBottomNav();
     }
 
     private void setupBottomNav() {
